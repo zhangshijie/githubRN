55969bba90968bdb0c57fd89d43c9d722d301496
CJ <cjzhang88@gmail.com>
2017-12-03 00:55:56 +0800

    commit

---
+++ App.js
@@ -0,0 +1,74 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View,
+  Image
+} from 'react-native';
+
+import TabNavigator from 'react-native-tab-navigator';
+
+const instructions = Platform.select({
+  ios: 'Press Cmd+R to reload,\n' +
+    'Cmd+D or shake for dev menu',
+  android: 'Double tap R on your keyboard to reload,\n' +
+    'Shake or press menu button for dev menu',
+});
+
+export default class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+     selectedTab :'home'
+    }
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <TabNavigator>
+          <TabNavigator.Item
+            selected={this.state.selectedTab === 'home'}
+            title="Home"
+            renderIcon={() => <Image source={require('./res/images/ic_favorite.png')} />}
+            renderSelectedIcon={() => <Image source={require('./res/images/ic_favorite.png')} />}
+            badgeText="1"
+            onPress={() => this.setState({ selectedTab: 'home' })}>
+            <View style={styles.page1}></View>
+          </TabNavigator.Item>
+          <TabNavigator.Item
+            selected={this.state.selectedTab === 'profile'}
+            title="Profile"
+            renderIcon={() => <Image source={require('./res/images/ic_trending.png')} />}
+            renderSelectedIcon={() => <Image source={require('./res/images/ic_trending.png')} />}
+            onPress={() => this.setState({ selectedTab: 'profile' })}>
+            <View style={styles.page2}></View>
+          </TabNavigator.Item>
+        </TabNavigator> 
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center'
+  },
+  page1: {
+    flex: 1,
+    backgroundColor: 'red'
+  },
+  page2: {
+    flex: 1,
+    backgroundColor: 'yellow'
+  },
+  
+});
